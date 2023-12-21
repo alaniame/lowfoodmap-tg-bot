70c5cbfecc49e98812d20ea04ed89620e3161dae
alaniame <alaniame@gmail.com>
2023-12-21 16:34:52 +0300

    new db

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -15,10 +15,28 @@ type Product struct {
 }
 
 func NewRepository(conn *pgx.Conn) Repository {
-	createTableSQL := `CREATE TABLE IF NOT EXISTS products (
+	createTableSQL := `CREATE TABLE IF NOT EXISTS product_categories (
+		category_id SERIAL PRIMARY KEY,
+		category_name VARCHAR(255) NOT NULL UNIQUE
+	);
+
+	CREATE TABLE IF NOT EXISTS carb_types (
+		carb_id SERIAL PRIMARY KEY,
+		carb_name VARCHAR(255) NOT NULL UNIQUE
+	);
+
+	CREATE TABLE IF NOT EXISTS products (
 		id SERIAL PRIMARY KEY,
-		product_name VARCHAR(50),
-		stage INT
+		product_name VARCHAR(255) NOT NULL UNIQUE,
+		category_id INT NOT NULL,
+		stage INT NOT NULL,
+		portion_high INT,
+		portion_medium INT,
+		portion_low INT,
+		portion_size VARCHAR(255),
+		carb_id INT,
+		FOREIGN KEY (category_id) REFERENCES product_categories (category_id),
+    	FOREIGN KEY (carb_id) REFERENCES carb_types (carb_id)
 	);`
 	_, createErr := conn.Exec(context.Background(), createTableSQL)
 	if createErr != nil {
